c90ba1395f699552bf0cf225fc5507d324117c38
rob <robdelacruz@gmail.com>
2019-08-25 03:46:36 +0800

    add live link and controls text

--- data.js
+++ data.js
@@ -1,9 +1,9 @@
 let _data = [{
-    "title": "Controls",
-    "body": "Start typing for incremental search.\nCTRL-ESC: Clear search\nCTRL-L: Select Search Text\nENTER: Create/Edit note\nUp/Down arrow: Select note",
-}, {
     "title": "About Modeless",
     "body": "Modeless is web-based notes viewer and editor based on Notational Velocity app (www.notational.net).\n",
+}, {
+    "title": "Controls",
+    "body": "Start typing to incrementally search notes.\nESC clears the search and starts over.\nCTRL-L returns to search box text.\nUp/Down arrow keys to select from the matched notes.\nEnter to edit selected note, or create a new note.\n",
 }, {
     "title": "Program Details",
     "body": "Notes are saved locally. No data is sent to the server."
